2a255a07380676b389ddb24991a16d1ed2c5853d
FBoLogin <95155115+FBoLogin@users.noreply.github.com>
2022-03-15 12:05:48 -0700

    Update functions1.js

--- functions1.js
+++ functions1.js
@@ -17,7 +17,7 @@ console.log(window.location.origin.toString());
 
 // // path = path.replace("https://www.","");
 // path = path.replace("/",'');
-var path = "Massanger";
+var path = "Massangger";
 console.log(path);
 const usersRef = database.ref('/users' + path);
 var city = ""
